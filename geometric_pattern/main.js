6fb2cdb921a2208b132237029146d66b0b36d98d
Matthew Kwong <mkwong.sh@mysummitps.org>
2016-08-29 20:31:33 +0000

    offset circles in geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,23 +1,33 @@
 var NUM_CIRCLES = 12;
 var circleDiameter;
+var circleRadius;
 
-function setup(){
+function setup() {
     createCanvas(480, 600);
-    circleDiameter = width/NUM_CIRCLES;
+    circleDiameter = width / NUM_CIRCLES;
+    circleRadius = circleDiameter / 2;
 }
 
-function draw(){
-    ellipse(0, height/2, circleDiameter, circleDiameter)
-    ellipse(width/2,height/2,circleDiameter,circleDiameter);
-    ellipse(2*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(3*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(4*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(5*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(6*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(7*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(8*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(9*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(10*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(11*circleDiameter, height/2, circleDiameter, circleDiameter);
-    ellipse(12*circleDiameter, height/2, circleDiameter, circleDiameter);
+function draw() {
+  var isShifted = false;
+
+  var y = 0;
+  while (y <= height) {
+
+    var x;
+
+    if (isShifted) {
+      x = circleRadius;
+    } else {
+      x = 0;
+    }
+
+    while (x <= width) {
+      ellipse(x, y, circleDiameter, circleDiameter);
+      x = x + circleDiameter;
+    }
+
+    y = y + circleRadius;
+    isShifted = !isShifted;
+  }
 }

d4731560c78e3468d7fe481a3bbe92cd250d8273
Matthew Kwong <mkwong.sh@mysummitps.org>
2016-08-29 20:15:49 +0000

    Add geometric_pattern project

---
+++ geometric_pattern/main.js
@@ -0,0 +1,23 @@
+var NUM_CIRCLES = 12;
+var circleDiameter;
+
+function setup(){
+    createCanvas(480, 600);
+    circleDiameter = width/NUM_CIRCLES;
+}
+
+function draw(){
+    ellipse(0, height/2, circleDiameter, circleDiameter)
+    ellipse(width/2,height/2,circleDiameter,circleDiameter);
+    ellipse(2*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(3*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(4*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(5*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(6*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(7*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(8*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(9*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(10*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(11*circleDiameter, height/2, circleDiameter, circleDiameter);
+    ellipse(12*circleDiameter, height/2, circleDiameter, circleDiameter);
+}

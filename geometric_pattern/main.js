be213c74238bd8540d7c9ee44eed9b8ce617cb89
Matthew Kwong <mkwong.sh@mysummitps.org>
2016-08-29 20:55:50 +0000

    Add save keybinding to geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,6 +1,10 @@
 var NUM_CIRCLES = 12;
 var circleDiameter;
 var circleRadius;
+var rVal;
+var gVal;
+var bVal;
+
 
 function setup() {
     createCanvas(480, 600);
@@ -9,28 +13,38 @@ function setup() {
 }
 
 function draw() {
-stroke(color(0, 255, 0));
+    rVal = 255;
+    gVal = 0;
+    bVal = 0;
 
-  var isShifted = false;
 
-  var y = 0;
-  while (y <= height) {
+    var isShifted = false;
 
-    var x;
+    var y = 0;
+    while (y <= height) {
 
-    if (isShifted) {
-      x = circleRadius;
-    } else {
-      x = 0;
+        var x;
+
+        if (isShifted) {
+            x = circleRadius;
+        }
+        else {
+            x = 0;
+        }
+
+        while (x <= width) {
+            ellipse(x, y, circleDiameter, circleDiameter);
+            x = x + circleDiameter;
+        }
+
+        y = y + circleRadius;
+        isShifted = !isShifted;
+
+
+        fill(color(rVal, gVal, bVal));
+        stroke(color(rVal, gVal, bVal));
+        rVal = rVal - 2;
+        gVal = gVal + 7;
+        bVal - bVal + 3;
     }
-
-    while (x <= width) {
-      ellipse(x, y, circleDiameter, circleDiameter);
-      x = x + circleDiameter;
-    }
-
-    y = y + circleRadius;
-    isShifted = !isShifted;
-  }
 }
-
